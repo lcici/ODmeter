f2951b1aff745534517351e639e3d7f50af1aa1b
lcici <changliu211@gmail.com>
2018-01-26 17:41:51 -0500

    Add files via upload

---
+++ Trigger_Camera.ino
@@ -0,0 +1,25 @@
+/*send the trigger for testing the camera
+ * Created by Chang Liu
+ * 2018-Jan-26
+ */
+ 
+const int triggerPin = 2; //trigger pin option
+
+void setup() {
+  // initialize the digital pin LED_BUILTIN as an output
+  // initialize the digital pin D2 as the trigger output
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(triggerPin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(triggerPin, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(triggerPin, LOW);
+  delay(2000);  
+
+}
